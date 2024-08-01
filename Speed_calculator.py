8cf6222d1940314b169895c3eb4dfbd8ca56366e
muneeb8715 <muneeb.8715@gmail.com>
2024-08-01 20:45:34 +0500

    Initial commit

---
+++ Speed_calculator.py
@@ -0,0 +1,41 @@
+from time import *
+import random as r
+
+def mistake(partest,usertest):
+    error = 0
+    for i in range(len(partest)):
+        try:
+            if partest[i] != usertest[i] :
+                error = error + 1
+        except:
+            error = error + 1
+    return error
+
+def speed_time(time_s,time_e,userinput):
+    time_delay = time_e - time_s
+    time_R = round(time_delay,2)
+    speed = len(userinput)/ time_R
+    return round(speed)
+
+if __name__=="__main__":
+    while True:
+        ck = input(" Ready to Test : Yes / No : ")
+        if ck == "yes" :
+            test = ["not any thing : no thing. leaves nothing to the imagination",
+            "value, or consequence. they mean nothing to me",
+            "Building a world where tech is fun again"]
+            test1 = r.choice(test)
+            print("***** Speed Testing *****")
+            print(test1)
+            print()
+            print()
+            time_1 = time()
+            testinput = input(" Enter : ")
+            time_2 = time()  
+            print("speed : ",speed_time(time_1,time_2,testinput),"w/sec" )
+            print("Error : ",mistake(test1,testinput))
+        elif ck == "no" :
+            print("Thank You")
+            break
+        else:
+            print(" Wromg Input ")
